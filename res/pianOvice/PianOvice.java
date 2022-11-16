05bedfe67e320e5073c6fc6e3734a290a03b2c42
Kien Nguyen <knguyen2@macalester.edu>
2022-11-16 13:53:15 -0600

    Create PianOvice.java

---
+++ res/pianOvice/PianOvice.java
@@ -0,0 +1,15 @@
+package pianOvice;
+
+import edu.macalester.graphics.*;
+
+public class PianOvice {
+    private CanvasWindow canvas;
+
+    public PianOvice() {
+        canvas = new CanvasWindow("PianOvice", 800, 600);
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
