3d7a3b4456c290db91b31eb6dfd5150ceccd0553
Kien Nguyen <knguyen2@macalester.edu>
2022-11-30 14:09:18 -0600

    added Pitch and copied some classes

--- src/pianOvice/Keyboard.java
+++ src/pianOvice/Keyboard.java
@@ -13,6 +13,9 @@ public class Keyboard {
     private List<String> blackNotes;
     private final int NUMBER_OF_WHITE_KEY = 14;
     private final int NUMBER_OF_BLACK_KEY = 24; 
+    private final int FIRST_PITCH = 48;
+    private List<Integer> blackKeyPositions = 
+        List.of(1, 3, 6, 8, 10, 13, 15, 18, 20, 22);
 
     public Keyboard(CanvasWindow canvas) {
         this.canvas = canvas;
@@ -21,21 +24,31 @@ public class Keyboard {
             "C4", "D4", "E4", "F4", "G4", "A4", "B4");
         blackNotes = List.of("C#3", "D#3", "F#3", "G#3", "A#3", 
             "C#4", "D#4", "F#4", "G#4", "A#4");
+        createKeys(startY);
+    }
+
+    private void createKeys(double startY) {
         createWhiteKeys(startY);
         createBlackKeys(startY);
     }
+
+    // (0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23)
 
     private void createWhiteKeys(double startY) {
         double whiteKeySize = canvas.getWidth() / NUMBER_OF_WHITE_KEY;
         double x = 0;
         double y = canvas.getHeight() - startY;
+        int pitchTracker = 0;
         for (int i = 0; i < NUMBER_OF_WHITE_KEY; i++) {
+            if (blackKeyPositions.contains(pitchTracker)) {
+                pitchTracker += 1;
+            }
             PianoKey key = new PianoKey(x, y, 
-                whiteKeySize, whiteKeySize * 4, Color.WHITE);
-            key.setNote(whiteNotes.get(i));
+                whiteKeySize, whiteKeySize * 4, Color.WHITE, whiteNotes.get(i), FIRST_PITCH + pitchTracker);
             canvas.add(key);
             keyList.add(key);
             x += whiteKeySize;
+            pitchTracker += 1;
         } 
     }
 
@@ -43,14 +56,11 @@ public class Keyboard {
         double blackKeySize = canvas.getWidth() / NUMBER_OF_BLACK_KEY;
         double x = 0;
         double y = canvas.getHeight() - startY;
-        List<Integer> blackKeyPositions = List.of
-            (1, 3, 6, 8, 10, 13, 15, 18, 20, 22);
         int noteTracker = 0;
         for (int i = 0; i < NUMBER_OF_BLACK_KEY; i++) {
             if (blackKeyPositions.contains(i)) {
                 PianoKey key = new PianoKey(x, y, 
-                    blackKeySize, blackKeySize * 4.5, Color.BLACK);
-                key.setNote(blackNotes.get(noteTracker));
+                    blackKeySize, blackKeySize * 4.5, Color.BLACK, blackNotes.get(noteTracker), FIRST_PITCH + i);
                 noteTracker += 1;
                 canvas.add(key);
                 keyList.add(key);
--- src/pianOvice/PianoKey.java
+++ src/pianOvice/PianoKey.java
@@ -6,12 +6,15 @@ import java.awt.Color;
 public class PianoKey extends Rectangle {
 
     private String note;
+    private double pitch;
 
-    public PianoKey(double x, double y, double width, double height, Color color) {
+    public PianoKey(double x, double y, double width, double height, Color color, String note, double pitch) {
         super(x, y, width, height);
         setFillColor(color);
         setStrokeColor(new Color(0x30808080));
         setStrokeWidth(3);
+        this.note = note;
+        this.pitch = pitch;
     }
         
     public String getNote() {
@@ -21,8 +24,17 @@ public class PianoKey extends Rectangle {
     public void setNote(String note) {
         this.note = note;
     }
-
-    public void OnClick() {
-        
+    
+    public double getPitch() {
+        return pitch;
     }
+
+    public void setPitch(double pitch) {
+        this.pitch = pitch;
+    }
+
+    // public double amplitudeAt(double t) {
+    //     double wavelength = Utils.convertPitchToWavelength(pitch);
+
+    // }
 }
